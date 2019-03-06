abee0bc4de8d8462193abf4637524abd332d4dec
Timo Freiberg <timo.freiberg@gmail.com>
2019-03-06 17:06:38 +0100

    Enable muuntaja middleware

--- src/clj/map_return_test/config.clj
+++ src/clj/map_return_test/config.clj
@@ -1,6 +1,7 @@
 (ns map-return-test.config
   (:require [environ.core :refer [env]]
-            [ring.middleware.defaults :refer [wrap-defaults api-defaults]]
+            muuntaja.middleware
+            [ring.middleware.defaults :refer [api-defaults wrap-defaults]]
             [ring.middleware.gzip :refer [wrap-gzip]]
             [ring.middleware.logger :refer [wrap-with-logger]]))
 
@@ -8,4 +9,5 @@
   {:http-port  (Integer. (or (env :port) 10555))
    :middleware [[wrap-defaults api-defaults]
                 wrap-with-logger
-                wrap-gzip]})
+                wrap-gzip
+                muuntaja.middleware/wrap-format]})
