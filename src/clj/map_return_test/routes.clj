8716411e1690e7ec9cf810ce8c3306968a88222b
Timo Freiberg <timo.freiberg@gmail.com>
2019-03-06 17:06:46 +0100

    Bug reproduction

--- src/clj/map_return_test/routes.clj
+++ src/clj/map_return_test/routes.clj
@@ -2,7 +2,7 @@
   (:require [clojure.java.io :as io]
             [compojure.core :refer [ANY GET PUT POST DELETE routes]]
             [compojure.route :refer [resources]]
-            [ring.util.response :refer [response]]))
+            [ring.util.response :as response :refer [response]]))
 
 (defn home-routes [endpoint]
   (routes
@@ -12,4 +12,41 @@
          io/input-stream
          response
          (assoc :headers {"Content-Type" "text/html; charset=utf-8"})))
+   (GET "/test" _
+     (response {:a "x"}))
+   (GET "/test-with-content-type" _
+     (-> (response {:a "x"})
+         (response/content-type "application/json")))
    (resources "/")))
+
+(def muuntaja-example
+  (muuntaja.middleware/wrap-format (fn [_]
+                                     (response {:a "x"}))))
+
+(def muuntaja-example-with-content-type
+  (muuntaja.middleware/wrap-format (fn [_]
+                                     (-> (response {:a "x"})
+                                         (response/content-type "application/json")))))
+
+(def request {:headers {"accept" "application/json"}})
+
+(comment
+  ;;works as expected, body is converted to JSON
+  (muuntaja-example request)
+
+  ;;leaves the body as is - didn't expect that but makes sense?
+  (muuntaja-example-with-content-type request)
+
+  ;;body not encoded, throws exception
+  ;;request is repeated (occurs in fresh chestnut project as well)
+  (clojure.java.shell/sh "curl"
+                         "-H"
+                         "accept: application/json"
+                         "localhost:10555/test")
+
+  ;;also fails with the exact same errors
+  (clojure.java.shell/sh "curl"
+                         "-H"
+                         "accept: application/json"
+                         "localhost:10555/test-with-content-type")
+  )
